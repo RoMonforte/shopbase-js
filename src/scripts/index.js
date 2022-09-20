b76e571a4cbbc1a0b126ed2614c66a5caef1ca93
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2022-09-19 19:07:44 -0500

    adding the products from JS to the html

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,7 @@ const cartIconMenu = document.querySelector('.navbar-shopping-cart');
 const mobileBurgerIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const asideProductDetail = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 mobileBurgerIcon.addEventListener('click', toggleMobileMenu);
@@ -33,3 +34,60 @@ function toggleCartAside() {
     asideProductDetail.classList.toggle('inactive'); 
 }
 
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push({
+    name: 'HD TV',
+    price: 7200,
+    image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push({
+    name: 'Sneakers',
+    price: 220,
+    image: 'https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+      
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+      
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+      
+        const productInfoDiv = document.createElement('div');
+      
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+      
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+      
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+      
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './Platzi_YardSale_Icons/bt_add_to_cart.svg');
+      
+       productInfoFigure.appendChild(productImgCart);
+       
+       productInfo.appendChild(productInfoDiv);
+       productInfo.appendChild(productInfoFigure);
+      
+       productCard.appendChild(productImg);
+       productCard.appendChild(productInfo);
+      
+       cardsContainer.appendChild(productCard);
+      }
+}
+
+renderProducts(productList);
